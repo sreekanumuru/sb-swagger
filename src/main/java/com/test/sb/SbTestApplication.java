6ac18bf88d2a5ec6675e946560dcfd921e099531
sreekanumuru <sreekanumuru@gmail.com>
2021-09-19 21:14:34 +0530

    added logs

--- src/main/java/com/test/sb/SbTestApplication.java
+++ src/main/java/com/test/sb/SbTestApplication.java
@@ -1,11 +1,24 @@
 package com.test.sb;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SbTestApplication {
+public static Logger log = LoggerFactory.getLogger(SbTestApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		log.info("Application started....");
+	}
+	
+	
 	public static void main(String[] args) {
+		log.info("Application executed....");
 		SpringApplication.run(SbTestApplication.class, args);
 	}
 }
--- src/test/java/com/test/sb/SbTestApplicationTests.java
+++ src/test/java/com/test/sb/SbTestApplicationTests.java
@@ -1,13 +1,19 @@
 package com.test.sb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SbTestApplicationTests {
-
+	public static Logger log = LoggerFactory.getLogger(SbTestApplicationTests.class);
 	@Test
 	void contextLoads() {
+		log.info("Test case executing....");
+		assertEquals(true, true);
 	}
 
 }
