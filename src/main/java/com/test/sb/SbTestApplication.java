e7d572dd47877a74c3ac9430d3b39c4c085ce57b
sreekanumuru <sreekanumuru@gmail.com>
2021-09-19 20:57:39 +0530

    removed empty lines

--- src/main/java/com/test/sb/SbTestApplication.java
+++ src/main/java/com/test/sb/SbTestApplication.java
@@ -5,11 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SbTestApplication {
-	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(SbTestApplication.class, args);
 	}
-
-	
 }
